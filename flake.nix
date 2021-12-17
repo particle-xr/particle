6e144efbd07c8025cad0ae8d1ea7bbb5bd38aa02
Will Ruggiano <wmruggiano@gmail.com>
2021-12-17 14:08:39 -0700

    feat: add localhost docs app

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    emanote.url = "github:srid/emanote";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, ... } @ inputs:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
@@ -15,6 +16,21 @@
           nativeBuildInputs = with pkgs; [
             nodejs
           ];
+        };
+
+        apps = {
+          docs =
+            let
+              emanote = inputs.emanote.defaultPackage."${system}";
+            in
+            utils.lib.mkApp {
+              drv = pkgs.writeShellApplication {
+                name = "eth-nix-docs";
+                text = ''
+                  PORT=''${1:-8080} ${emanote}/bin/emanote -L ./docs
+                '';
+              };
+            };
         };
       });
 }
